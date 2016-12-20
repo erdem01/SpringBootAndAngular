4f83fce66ca23e4a035182dc6217662c0c8cbff1
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2016-12-20 23:59:04 +0200

    some scripts corrected

--- SpringAngularTogether1/src/main/resources/static/app.js
+++ SpringAngularTogether1/src/main/resources/static/app.js
@@ -1,23 +1,3 @@
 (function() {
 	var module = angular.module('HomePageModule', ['RouterModule', 'AuthenticationModule', 'LoginPageModule', 'HelloPageModule']);
-	module.config(['$routeProvider', 'loginPath', 'helloPath', function($routeProvider, loginPath, helloPath) {
-		$routeProvider
-		
-        .when(helloPath, {
-        	controller: 'HelloController',
-        	templateUrl: 'pages/hello/hello.view.html',
-            controllerAs: 'ctrl'
-        })
-
-        .when(loginPath, {
-            controller: 'LoginController',
-            templateUrl: 'pages/login/login.view.html',
-            controllerAs: 'ctrl'
-        })
-
-        .otherwise({ redirectTo: loginPath });
-	}]);
-	module.config(['$httpProvider', function($httpProvider) {
-		$httpProvider.interceptors.push('AuthInterceptor');
-	}]);
 })();
--- SpringAngularTogether1/src/main/resources/static/services/router.js
+++ SpringAngularTogether1/src/main/resources/static/services/router.js
@@ -16,4 +16,24 @@
 			redirectToHello: redirectToHello
 		};
 	}]);
+	module.config(['$routeProvider', 'loginPath', 'helloPath', function($routeProvider, loginPath, helloPath) {
+		$routeProvider
+		
+        .when(helloPath, {
+        	controller: 'HelloController',
+        	templateUrl: 'pages/hello/hello.view.html',
+            controllerAs: 'ctrl'
+        })
+
+        .when(loginPath, {
+            controller: 'LoginController',
+            templateUrl: 'pages/login/login.view.html',
+            controllerAs: 'ctrl'
+        })
+
+        .otherwise({ redirectTo: loginPath });
+	}]);
+	module.config(['$httpProvider', function($httpProvider) {
+		$httpProvider.interceptors.push('AuthInterceptor');
+	}]);
 })();
