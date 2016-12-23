889f5b8963a29977117f3fba5afe082a548138c1
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2016-12-23 23:13:36 +0200

    SpringAngularTogether1 project is now a deployable war file.

--- SpringAngularTogether1/src/main/java/erdemc/SpringAngularTogether1Application.java
+++ SpringAngularTogether1/src/main/java/erdemc/SpringAngularTogether1Application.java
@@ -2,11 +2,20 @@ package erdemc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringAngularTogether1Application {
+public class SpringAngularTogether1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAngularTogether1Application.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringAngularTogether1Application.class);
+    }
+
+    
 }
