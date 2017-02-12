4ec87f27a7ac6cb5d732b24943e0e50a405e72fb
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2017-02-12 15:02:45 +0200

    Role in spring configuration has been corrected.

--- SpringAngularTogether1/src/main/java/erdemc/config/SpringAngularTogether1WebConfig.java
+++ SpringAngularTogether1/src/main/java/erdemc/config/SpringAngularTogether1WebConfig.java
@@ -3,10 +3,8 @@ package erdemc.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,7 +36,8 @@ public class SpringAngularTogether1WebConfig extends WebSecurityConfigurerAdapte
          .authenticationEntryPoint(authEntryPoint())
          .and()
          .authorizeRequests()
-         .antMatchers("/**").hasAnyRole("ROLE_USER")
+         .antMatchers("/**")
+         .hasRole("USER")
          .and()
          .formLogin()
          .usernameParameter("username")
@@ -48,11 +47,6 @@ public class SpringAngularTogether1WebConfig extends WebSecurityConfigurerAdapte
          .and()
          .logout();
 	}
-	
-	@Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
-    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder(){
