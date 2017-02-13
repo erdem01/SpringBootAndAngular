167c80de39bcc44ca7ab41e3d2d7b5f1194b231e
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2017-02-14 01:13:40 +0200

    Responsive order page implemented.

--- AngularFrontEndForSpring1/src/main/resources/static/pages/order/orderPage.js
+++ AngularFrontEndForSpring1/src/main/resources/static/pages/order/orderPage.js
@@ -2,19 +2,37 @@
 	var module = angular.module('OrderPageModule', ['CoffeeModule']);
 	module.controller('OrderPageController', ['$log', 'CoffeeService', function($log, CoffeeService) {
 		var self = this;
-		self.coffees = [];
+		var coffees = [];
 		self.getCoffees = function() {
-			return self.coffees;
+			return coffees;
 		};
 		var refreshCoffees = function() {
 			var coffeePromise = CoffeeService.gatherCoffees();
 			coffeePromise.then(function(response) {
-				self.coffees = response;
+				coffees = response;
 				$log.log(response);
 			}, function(errorMessage) {
 				$log.log(errorMessage);
 			});
 		};
 		refreshCoffees();
+		var orders = [];
+		self.addOrder = function(coffee) {
+			for(var i=0; i<orders.length; i++) {
+				var order = orders[i];
+				if(order.coffee.id == coffee.id) {
+					order.count++;
+					return;
+				}
+			}
+			var newOrder = {
+				count: 1,
+				coffee: coffee
+			};
+			orders.push(newOrder);
+		}
+		self.getOrders = function() {
+			return orders;
+		}
 	}]);
 })();
