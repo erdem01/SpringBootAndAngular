231c83ddebc2e01397f8aefc50961a8133ffa1ea
Erdem Caglayan <erdemcaglayan_1@hotmail.com>
2017-05-03 08:17:38 +0300

    Unfinished test case.

--- CoffeeShopWeb/test/testScripts/services/AuthenticationServiceSpec.js
+++ CoffeeShopWeb/test/testScripts/services/AuthenticationServiceSpec.js
@@ -1,10 +1,21 @@
 describe('AuthenticationService test', function() {
 	var $httpBackend, AuthenticationService;
+	var arrangeResult;
 	beforeEach(module('AuthenticationModule'));
 	beforeEach(inject(function(_$httpBackend_, _AuthenticationService_) {
 		$httpBackend = _$httpBackend_;
 		AuthenticationService = _AuthenticationService_;
+		arrangeResult = function(username, password, respCode, resultObj) {
+			var postData = 'username=' + username + '&password=' + password;
+			$httpBackend.expectPOST("/coffeeShopServ/login", postData, function(headers) {
+				return headers['Content-Type'] === 'application/x-www-form-urlencoded;charset=utf-8';
+			}).respond(respCode, resultObj);
+		};
 	}));
 	it('should execute login successfully!', function() {
+	});
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
 	});
 });
